f60348670b2b8637d2506da3e7deb8351291cc28
Thanh Nguyen Dac <thanh4890@gmail.com>
2024-12-10 17:45:46 +0700

    style menu and buttons

--- game/src/Game/Button.js
+++ game/src/Game/Button.js
@@ -2,7 +2,7 @@ import { StyledButton } from "./Button.style";
 
 const Button = ({ icon, value, onSelect }) => {
   return (
-    <StyledButton onClick={onSelect}>
+    <StyledButton onClick={onSelect} value={value}>
       <img src={icon} alt={value} />
     </StyledButton>
   );
