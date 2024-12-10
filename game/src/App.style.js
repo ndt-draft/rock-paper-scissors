e356f630d44458fa4852a2aeb903ccc1d63ac668
Thanh Nguyen Dac <thanh4890@gmail.com>
2024-12-10 15:32:31 +0700

    add styled-components, copy game assets add some basic App, Header style

---
+++ game/src/App.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledApp = styled.div`
+  background-color: hsl(214, 47%, 23%);
+  height: 100vh;
+
+  .game-wrapper {
+    max-width: 1366px;
+    margin: auto;
+  }
+`;
---
+++ game/src/Header/index.js
@@ -0,0 +1,11 @@
+import logo from "../images/logo-bonus.svg";
+
+const Header = () => {
+  return (
+    <div>
+      <img src={logo} alt="logo" />
+    </div>
+  );
+};
+
+export default Header;
