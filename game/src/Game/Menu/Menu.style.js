14cb897a38d754a7052bc393f60fcb5f530da73e
Thanh Nguyen Dac <thanh4890@gmail.com>
2024-12-10 22:59:44 +0700

    update menu pentagon background image

--- game/src/Game/Menu/Menu.style.js
+++ game/src/Game/Menu/Menu.style.js
@@ -1,8 +1,10 @@
 import styled from "styled-components";
+import pentagon from "../../images/bg-pentagon.svg";
 
 export const StyledMenu = styled.div`
   max-width: 500px;
   height: 500px;
   margin: auto;
   position: relative;
+  background: url(${pentagon}) no-repeat center 17px;
 `;
