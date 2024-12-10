683a2eeaf9a903d991ea96d6caa063e2f8a18b38
Thanh Nguyen Dac <thanh4890@gmail.com>
2024-12-10 16:40:42 +0700

    add base game logic

--- game/src/Game/Menu.js
+++ game/src/Game/Menu.js
@@ -1,5 +1,31 @@
+import rockIcon from "../images/icon-rock.svg";
+import paperIcon from "../images/icon-paper.svg";
+import scissorsIcon from "../images/icon-scissors.svg";
+import { useGame } from "../GameProvider";
+
+const selections = [
+  { value: "rock", icon: rockIcon },
+  { value: "paper", icon: paperIcon },
+  { value: "scissors", icon: scissorsIcon },
+];
+
 const Menu = () => {
-  return <div>Menu</div>;
+  const { handleUserSelect, userSelection, pcSelection, status, point } =
+    useGame();
+
+  return (
+    <div>
+      {selections.map((selection) => (
+        <button onClick={handleUserSelect(selection.value)}>
+          <img src={selection.icon} alt={selection.value} />
+        </button>
+      ))}
+      {userSelection}
+      {pcSelection}
+      {status}
+      {point}
+    </div>
+  );
 };
 
 export default Menu;
--- game/src/GameProvider/index.js
+++ game/src/GameProvider/index.js
@@ -1,22 +1,41 @@
 import React, { useContext, useReducer } from "react";
+import lodash from "lodash";
 
-const GameContext = React.createContext({
+const initialState = {
   screen: "menu",
-});
+  status: null,
+  userSelection: null,
+  pcSelection: null,
+  point: 0,
+};
+
+const GameContext = React.createContext(initialState);
 
 export const useGame = () => useContext(GameContext);
 
-function reducer(state, action) {
+function gameReducer(state, action) {
   switch (action.type) {
     case "change_screen":
       return {
         screen: action.payload,
       };
+    case "update_game": {
+      return {
+        ...state,
+        ...action.payload,
+      };
+    }
   }
 }
 
+const winConditions = {
+  rock: ["scissors"],
+  paper: ["rock"],
+  scissors: ["paper"],
+};
+
 const GameProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, { screen: "menu" });
+  const [state, dispatch] = useReducer(gameReducer, initialState);
 
   const changeScreen = (newScreen) => {
     dispatch({
@@ -25,8 +44,33 @@ const GameProvider = ({ children }) => {
     });
   };
 
+  const handleUserSelect = (userSelection) => () => {
+    const pcSelection = lodash.sample(["rock", "paper", "scissors"]);
+    const isUserWon = lodash.includes(
+      winConditions[userSelection],
+      pcSelection
+    );
+    const gameStatus =
+      userSelection === pcSelection ? "draw" : isUserWon ? "win" : "lose";
+
+    dispatch({
+      type: "update_game",
+      payload: {
+        status: gameStatus,
+        userSelection,
+        pcSelection,
+        point: gameStatus === "win" ? state.point + 1 : state.point,
+      },
+    });
+  };
+
+  const actions = {
+    changeScreen,
+    handleUserSelect,
+  };
+
   return (
-    <GameContext.Provider value={{ ...state, dispatch, changeScreen }}>
+    <GameContext.Provider value={{ ...state, dispatch, ...actions }}>
       {children}
     </GameContext.Provider>
   );
